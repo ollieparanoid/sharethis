412f5c22c6466241539f47269ae54339a940afab
Oliver Smith <ollieparanoid@bitmessage.ch>
2015-09-06 14:54:00 +0200

    add empty nginx config, test nginx config in sharethis.sh

--- sharethis.sh
+++ sharethis.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+WEBROOT="$(pwd -P)"
+SHARETHIS="$(cd $(dirname $0); pwd -P)"
+NGINX="nginx -c $SHARETHIS/nginx.conf"
+CACHE=~/.cache/sharethis
+
 # Check dependencies
 DEPENDENCIES="openssl nginx fcgiwrap firejail sha512sum"
 for DEPENDENCY in $DEPENDENCIES; do
@@ -11,19 +16,18 @@ for DEPENDENCY in $DEPENDENCIES; do
 	exit 1
 done
 
-
-SHARETHIS="$(cd $(dirname $0); pwd -P)"
-CACHE=~/.cache/sharethis
-
 # Generate certificate
 if [ ! -e "$CACHE/server.key" ]; then
-	mkdir -p $CACHE
-	cd $CACHE
+	mkdir -p "$CACHE"
+	cd "$CACHE"
 	openssl req -x509 -newkey rsa:4096 -keyout server.key \
 		-out server.cert -days 365 -sha512 -subj "/CN=localhost" \
 		-nodes || rm -r "$CACHE"
 	
 	[ ! -e "$CACHE/server.key" ] && exit 2
 fi
+
+# Verify nginx.conf
+$NGINX -t -q || exit 3
 
 echo "STUB"
