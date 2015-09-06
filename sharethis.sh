c3e68b3476ad20eef2eecd6a0ad55d118db4ea8a
Oliver Smith <ollieparanoid@bitmessage.ch>
2015-09-06 02:53:16 +0200

    automatically generate https certificate

--- sharethis.sh
+++ sharethis.sh
@@ -10,3 +10,20 @@ for DEPENDENCY in $DEPENDENCIES; do
 	echo "  "$DEPENDENCIES
 	exit 1
 done
+
+
+SHARETHIS="$(cd $(dirname $0); pwd -P)"
+CACHE=~/.cache/sharethis
+
+# Generate certificate
+if [ ! -e "$CACHE/server.key" ]; then
+	mkdir -p $CACHE
+	cd $CACHE
+	openssl req -x509 -newkey rsa:4096 -keyout server.key \
+		-out server.cert -days 365 -sha512 -subj "/CN=localhost" \
+		-nodes || rm -r "$CACHE"
+	
+	[ ! -e "$CACHE/server.key" ] && exit 2
+fi
+
+echo "STUB"
